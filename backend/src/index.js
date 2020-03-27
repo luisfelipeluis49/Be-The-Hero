771f6da18effe7a8888095b2015f9d5632132aff
Luis Felipe <luisfelipeluis49@gmail.com>
2020-03-27 18:23:16 -0300

    Backend da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,28 +10,3 @@ app.use(routes);
 
 app.listen(3333);
 
-// 
-// Rota / Recurso
-//
-
-//
-// Métodos HTTP:
-//
-// GET: Buscar/listar uma informação do backend
-// POST: Criar uma informação do backend
-// PUT: Alterar uma inforrmação do backend
-// DELETE: Deletar uma informação do backend
-// 
-
-//
-// Tipos de parâmetros:
-//
-// Query Params: Parâmetros  enviados na rota após "?" (Filtros, paginação)
-// Route Params: Parâmetros utilizados para identificar recursos 
-// Request Body: Corpo da tequisição, utilizado para criar ou alterar recursos
-//
-
-//
-// Driver: SELECT * FROM users
-// Query Builder: table('users').select('*').where()
-//
